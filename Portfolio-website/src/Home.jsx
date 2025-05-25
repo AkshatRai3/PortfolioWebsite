338143ac6a2f8cdb92f7001016cf96f4db9be72b
Akshat Rai <raiakshat0003@gmail.com>
2025-05-25 21:19:39 +0530

    completed home page

---
+++ Portfolio-website/src/Home.jsx
@@ -0,0 +1,51 @@
+import './index.css'
+
+
+function Home(){
+    return (
+        <>
+        <div class="macbook-air-1">
+            <div class="macbook-air-1-child">
+				<div class="rai">RAI</div>
+				<div class="home">HOME</div>
+				<div class="about">ABOUT</div>
+				<div class="works">WORKS</div>
+				<div class="contact">CONTACT</div>
+    		</div>
+    		<div class="namaste">NAMASTE</div>
+    		<div class="welcome-to-the-container">
+      			<p class="welcome-to-the-world-of">
+        				<span>
+          					<span>Welcome to the world of</span>
+          					<span class="span"> </span>
+        				</span>
+      			</p>
+      			<p class="akshat-rai">
+        				<span class="akshat-rai1">AKSHAT RAI</span>
+        				<span>
+          					<span>,</span>
+        				</span>
+      			</p>
+      			<p class="welcome-to-the-world-of">
+        				<span>
+          					<span>&nbsp;</span>
+        				</span>
+      			</p>
+      			<p class="welcome-to-the-world-of">
+        				<span>
+          					<span>Where logic dances with creativity.</span>
+        				</span>
+      			</p>
+    		</div>
+    		<img class="whatsapp-image-2025-05-25-at-1" alt="" src="WhatsApp Image 2025-05-25 at 15.57.17 1.png"/>
+    		
+    		
+			<div class="i-build-websites">I build websites</div>
+    		
+  	</div>
+        
+        </>
+    )
+}
+
+export default Home;
