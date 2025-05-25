32dfe59bfb044cf3246fa8881b78e3df91e71c04
Akshat Rai <raiakshat0003@gmail.com>
2025-05-26 01:55:16 +0530

    making home page responsive

--- Portfolio-website/src/Home.jsx
+++ Portfolio-website/src/Home.jsx
@@ -6,37 +6,41 @@ function Home(){
         <>
         <div class="macbook-air-1">
             <div class="macbook-air-1-child">
-				<div class="rai">RAI</div>
-				<div class="home">HOME</div>
-				<div class="about">ABOUT</div>
-				<div class="works">WORKS</div>
-				<div class="contact">CONTACT</div>
+				<a href="youtube.com"><div class="rai">RAI</div></a>
+				<a href="#"><div class="home">HOME</div></a>
+				<a href="#"><div class="about">ABOUT</div></a>
+				<a href="#"><div class="works">WORKS</div></a>
+				<a href="#"><div class="contact">CONTACT</div></a>
     		</div>
-    		<div class="namaste">NAMASTE</div>
-    		<div class="welcome-to-the-container">
-      			<p class="welcome-to-the-world-of">
-        				<span>
-          					<span>Welcome to the world of</span>
-          					<span class="span"> </span>
-        				</span>
-      			</p>
-      			<p class="akshat-rai">
-        				<span class="akshat-rai1">AKSHAT RAI</span>
-        				<span>
-          					<span>,</span>
-        				</span>
-      			</p>
-      			<p class="welcome-to-the-world-of">
-        				<span>
-          					<span>&nbsp;</span>
-        				</span>
-      			</p>
-      			<p class="welcome-to-the-world-of">
-        				<span>
-          					<span>Where logic dances with creativity.</span>
-        				</span>
-      			</p>
-    		</div>
+            <div className='main-text'>
+
+                <div class="namaste">NAMASTE</div>
+                <div class="welcome-to-the-container">
+                    <p class="welcome-to-the-world-of text">
+                            <span>
+                                <span>Welcome to the world of</span>
+                                <span class="span"> </span>
+                            </span>
+                    </p>
+                    <p class="akshat-rai text">
+                            <span class="akshat-rai1">AKSHAT RAI</span>
+                            <span>
+                                <span>,</span>
+                            </span>
+                    </p>
+                    <p class="welcome-to-the-world-of text">
+                            <span>
+                                <span>&nbsp;</span>
+                            </span>
+                    </p>
+                    <p class="welcome-to-the-world-of text">
+                            <span>
+                                <span>Where logic dances with creativity.</span>
+                            </span>
+                    </p>
+                </div>
+
+            </div>
     		<img class="whatsapp-image-2025-05-25-at-1" alt="" src= {image} />
     		
     		
